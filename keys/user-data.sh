60f99b6418cfd63990a68b314cd2572c1d76073c
Robert Wang <rwang5688@outlook.com>
2021-04-24 21:25:25 -0700

    add user data script for Route 53 labs

---
+++ keys/user-data.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+yum update -y
+yum install httpd -y
+cd /var/www/html
+echo "Hello from Web Server 1 in us-east-2 N. Virginia" > index.html
+echo "Hello from Web Server 2 in us-east-2 N. Virginia" > index.html
+echo "Hello from Web Server 3 in ap-southeast-2 Sydney" > index.html
+echo "Hello from Web Server 4 in eu-north-1 Stockholm" > index.html
+service httpd start
+chkconfig httpd start
+sudo yum install -y perl-Switch perl-DateTime perl-Sys-Syslog perl-LWP-Protocol-https perl-Digest-SHA.x86_64
+cd /home/ec2-user/
+curl https://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.2.zip -O
+unzip CloudWatchMonitoringScripts-1.2.2.zip
+rm -rf CloudWatchMonitoringScripts-1.2.2.zip
+
+#   Use the commands below for the lab.
+#   /home/ec2-user/aws-scripts-mon/mon-put-instance-data.pl --mem-util --verify --verbose
+#   /home/ec2-user/aws-scripts-mon/mon-put-instance-data.pl --mem-util --mem-used --mem-avail
+#   */1 * * * * root /home/ec2-user/aws-scripts-mon/mon-put-instance-data.pl --mem-util --mem-used --mem-avail
+
+
