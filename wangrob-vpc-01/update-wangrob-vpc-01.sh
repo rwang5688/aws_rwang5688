3047391774ce897bc29479b39b5f984946c81d62
Robert Wang <rwang5688@outlook.com>
2021-05-05 23:45:49 -0700

    add wangrob-vpc-01 lab baseline

---
+++ wangrob-vpc-01/update-wangrob-vpc-01.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+aws cloudformation update-stack --stack-name wangrob-vpc-01 \
+--template-body file://wangrob-vpc-01.yaml \
+--capabilities CAPABILITY_NAMED_IAM \
+--parameters file://wangrob-vpc-01-parameters.json \
+--region us-west-2
+
