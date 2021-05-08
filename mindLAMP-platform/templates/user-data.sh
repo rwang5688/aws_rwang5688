a53e73d35b48f2b086562aa7c7b8659d59d60126
Robert Wang <rwang5688@outlook.com>
2021-05-08 02:38:23 -0700

    try cp -R  aws-scripts-mon/ /home/ssm-user/

--- mindLAMP-platform/templates/user-data.sh
+++ mindLAMP-platform/templates/user-data.sh
@@ -22,7 +22,7 @@ cd /home/ec2-user/
 curl https://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.2.zip -O
 unzip CloudWatchMonitoringScripts-1.2.2.zip
 rm -rf CloudWatchMonitoringScripts-1.2.2.zip
-cp -r aws-scripts-mon /home/ssm-user/
+cp -R aws-scripts-mon/ /home/ssm-user/
 
 #   Use the commands below to enable memory metrics monitoring in CloudWatch
 #   /home/ec2-user/aws-scripts-mon/mon-put-instance-data.pl --mem-util --mem-used --mem-avail --verify --verbose
