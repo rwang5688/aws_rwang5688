a9b3bb88bfe05ba2a204aed214ef1b7c5c4c1b1a
Robert Wang <rwang5688@outlook.com>
2021-05-10 13:37:39 -0700

    add v1 to templates env var and stack-names

--- mindLAMP-platform-v1/scripts/delete-mindLAMP-platform-v1.sh
+++ mindLAMP-platform-v1/scripts/delete-mindLAMP-platform-v1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-export MINDLAMP_PLATFORM_TEMPLATES=$MINDLAMP_PLATFORM_HOME/templates
+export MINDLAMP_PLATFORM_V1_TEMPLATES=$MINDLAMP_PLATFORM_V1_HOME/templates
 
 # print environment variables
-echo "MINDLAMP_PLATFORM_TEMPLATES=$MINDLAMP_PLATFORM_TEMPLATES"
+echo "MINDLAMP_PLATFORM_V1_TEMPLATES=$MINDLAMP_PLATFORM_V1_TEMPLATES"
 
 # execute
 echo "delete-mindLAMP-platform in region=$1 with configuration=$2"
-aws cloudformation delete-stack --stack-name mindLAMP-platform-$1-$2 \
+aws cloudformation delete-stack --stack-name mindLAMP-platform-v1-$1-$2 \
 --region $1
 
--- mindLAMP-platform-v1/scripts/validate-mindLAMP-platform-v1.sh
+++ mindLAMP-platform-v1/scripts/validate-mindLAMP-platform-v1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-export MINDLAMP_PLATFORM_TEMPLATES=$MINDLAMP_PLATFORM_HOME/templates
+export MINDLAMP_PLATFORM_V1_TEMPLATES=$MINDLAMP_PLATFORM_V1_HOME/templates
 
 # print environment variables
-echo "MINDLAMP_PLATFORM_TEMPLATES=$MINDLAMP_PLATFORM_TEMPLATES"
+echo "MINDLAMP_PLATFORM_V1_TEMPLATES=$MINDLAMP_PLATFORM_V1_TEMPLATES"
 
 # execute
 echo "validate-mindLAMP-platform"
-aws cloudformation validate-template --template-body file://$MINDLAMP_PLATFORM_TEMPLATES/mindLAMP-platform-v1.yaml
+aws cloudformation validate-template --template-body file://$MINDLAMP_PLATFORM_V1_TEMPLATES/mindLAMP-platform-v1.yaml
 
